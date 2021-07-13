d54e5bc17b51659b37cbdf52050da8c4dedf3ec6
boo-learn <t89236238012@gmail.com>
2021-07-13 15:43:25 +0300

    improved Module-6

--- Module6/examples/4_file_json.py
+++ Module6/examples/4_file_json.py
@@ -5,16 +5,17 @@ DIR = 'files'
 python_data = [{"name": 'Петр', 'age': 10}, {"name": 'Александр', 'age': 8}]
 
 # Записываем объект Python в файл в виде JSON
-with open(os.path.join(DIR, 'save_json_data.json'), 'w', encoding='UTF-8', ) as f:
+path = os.path.join(DIR, 'save_json_data.json')
+with open(path, 'w', encoding='UTF-8', ) as f:
     json.dump(python_data, f, ensure_ascii=False)
     # ensure_ascii=False - чтобы некирилические символы не были преобразованы к unicode-последовательности
 
 # Читаем JSON из файла и преобразуем к типу Python
-with open(os.path.join(DIR, 'save_json_data.json'), 'r', encoding='UTF-8') as f:
+with open(path, 'r', encoding='UTF-8') as f:
     read_data = json.load(f)
 
-print(read_data)
-print(read_data[0])
+print("type = ", type(read_data))
+print("data = ", read_data)
 
 # Плюсы: Легко читаемая и редактируемая информация в файле
 #        JSON можно передавать различным программам на других языках
