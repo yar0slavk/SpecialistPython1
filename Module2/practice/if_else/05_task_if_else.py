3c7900fea3d6f3ae88e66a77008b880e2b45b7be
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-07 16:13:46 +0300

    Update 05_task_if_else.py

--- Module2/practice/if_else/05_task_if_else.py
+++ Module2/practice/if_else/05_task_if_else.py
@@ -5,29 +5,11 @@
 
 m = int(input('Input month (num): '))
 
-if m == 1:
-    print('January')
-elif m == 2:
-    print('February')
-elif m == 3:
-    print('March')
-elif m == 4:
-    print('April')
-elif m == 5:
-    print('May')
-elif m == 6:
-    print('June')
-elif m == 7:
-    print('July')
-elif m == 8:
-    print('August')
-elif m == 9:
-    print('September')
-elif m == 10:
-    print('October')
-elif m == 11:
-    print('November')
-elif m == 12:
-    print('December')
+if 3 <= m <= 5:
+    print('Spring')
+elif 6 <= m <= 8:
+    print('Summer')
+elif 9 <= m <= 11:
+    print('Autumn')
 else:
-    print('Not valid month')
+    print('Winter')
