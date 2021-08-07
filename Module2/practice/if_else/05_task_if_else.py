9eef8fa3645ad4b8f61647fa827410ea346f4d86
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-07 15:57:23 +0300

    Update 05_task_if_else.py

--- Module2/practice/if_else/05_task_if_else.py
+++ Module2/practice/if_else/05_task_if_else.py
@@ -3,4 +3,31 @@
 # Формат входных данных: дано целое число m (1 ≤ m ≤ 12).
 # Формат выходных данных: требуется вывести название времени года
 
-# TODO: your code here
+m = int(input('Input month (num): '))
+
+if m == 1:
+    print('January')
+elif m == 2:
+    print('February')
+elif m == 3:
+    print('March')
+elif m == 4:
+    print('April')
+elif m == 5:
+    print('May')
+elif m == 6:
+    print('June')
+elif m == 7:
+    print('July')
+elif m == 8:
+    print('August')
+elif m == 9:
+    print('September')
+elif m == 10:
+    print('October')
+elif m == 11:
+    print('November')
+elif m == 12:
+    print('December')
+else:
+    print('Not valid month')
