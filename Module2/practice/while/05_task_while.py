3231692b6bd3d548f58ae0b831be9ca21e411a1b
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-14 12:21:59 +0300

    Update 05_task_while.py

--- Module2/practice/while/05_task_while.py
+++ Module2/practice/while/05_task_while.py
@@ -16,5 +16,33 @@
  #
 # #
 
+SIGN = '#'
+a = int(input('Input a (2 < a <= 30): '))
 
-# TODO: your code here
+# 2 cycles
+i = 1
+while i <= a:
+    tmp = ''
+    j = 1
+    while j <= a:
+        if j == i or j == a-i+1:
+            tmp += SIGN
+        else:
+            tmp += ' '
+        j += 1
+    print(tmp)
+    i += 1
+
+# 1 cycle
+tmp = ''
+i = 1
+while i <= a ** 2:
+    if i == a*((i-1)//a) + (i-1)//a + 1 or i == ((i-1)//a+1)*(a-1) + 1:
+        tmp += SIGN
+    else:
+        tmp += ' '
+        
+    if i % a == 0:
+        print(tmp)
+        tmp = ''
+    i += 1
