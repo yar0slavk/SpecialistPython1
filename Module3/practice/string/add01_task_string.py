0f776433655e5dc876f1803d537674b376f33f30
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-17 22:12:20 +0300

    Update add01_task_string.py

--- Module3/practice/string/add01_task_string.py
+++ Module3/practice/string/add01_task_string.py
@@ -2,9 +2,21 @@
 # В предложении могут присутствовать следующие знаки препинания ",.!?-". Знаки препинания частьюслова не являются.
 # Определить в предоставленном сообщении количество слов длиной больше, чем 7.
 
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat consequat vestibulum. " \
-       "Donec tincidunt sed lorem et feugiat. Nullam elementum"
+LENGTH = 7
+TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pla-erat consequat vestibulum. " \
+       "Donec tincidunt sed lo-rem et feug-iat. Nullam eleme-ntum"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 # Примечание: обратите внимание на перенос длинной строки на новую строку
 
-# TODO: your code here
+tmp_str = TEXT
+punct = ('.', ',', '!', '?', '-')
+
+for sign in punct:
+    tmp_str = tmp_str.replace(sign, '')
+
+num_words = 0
+for word in tmp_str.split(' '):
+    if len(word) > LENGTH:
+        num_words += 1
+
+print('number of words(len='+str(LENGTH)+'):', num_words)
