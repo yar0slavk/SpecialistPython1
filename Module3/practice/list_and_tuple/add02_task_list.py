1e47a23b4704278eedeaf69575181ebd6e16455d
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-17 22:14:20 +0300

    Update add02_task_list.py

--- Module3/practice/list_and_tuple/add02_task_list.py
+++ Module3/practice/list_and_tuple/add02_task_list.py
@@ -5,3 +5,13 @@
 # Примечание: для экономии времени, можно ограничить номер дня десятью.
 # Примечание: склонениями названий можно принебречь
 
+DAYS = ('first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
+        'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth',
+        'nineteenth', 'twentieth', 'twenty-first', 'twenty-second', 'twenty-third', 'twenty-fourth', 'twenty-fifth',
+        'twenty-sixth', 'twenty-seventh', 'twenty-eighth', 'twenty-ninth', 'thirtieth', 'thirty-first')
+MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+          'November', 'December')
+
+date = input('Input date (dd.mm.yyyy): ')
+dd, mm, yyyy = date.split('.')
+print(f'{DAYS[int(dd) - 1]} of {MONTHS[int(mm) - 1]}, {yyyy}')
