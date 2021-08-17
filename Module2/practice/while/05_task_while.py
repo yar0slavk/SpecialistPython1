5f44f268382eb1737c7ee2790fe68ef126d257f8
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-17 22:06:19 +0300

    Update 05_task_while.py

--- Module2/practice/while/05_task_while.py
+++ Module2/practice/while/05_task_while.py
@@ -16,5 +16,47 @@
  #
 # #
 
+SIGN = '#'
+a = int(input('Input a (2 < a <= 30): '))
+# variant1
+print('2 cycles 1..a')
+i = 1
+while i <= a:
+    tmp = ''
+    j = 1
+    while j <= a:
+        if j == i or j == a - i + 1:
+            tmp += SIGN
+        else:
+            tmp += ' '
+        j += 1
+    print(tmp)
+    i += 1
+# variant2
+print('\n1 cycle 1..a**2')
+tmp = ''
+i = 1
+while i <= a ** 2:
+    if i == a * ((i - 1) // a) + (i - 1) // a + 1 or i == ((i - 1) // a + 1) * (a - 1) + 1:
+        tmp += SIGN
+    else:
+        tmp += ' '
 
-# TODO: your code here
+    if i % a == 0:
+        print(tmp)
+        tmp = ''
+    i += 1
+# variant3
+print('\n1 cycle 1..a')
+num_spaces = 0
+i = 1
+while i <= a:
+    tmp = SIGN
+    if i <= a / 2:
+        num_spaces = a - i * 2
+    else:
+        num_spaces = i * 2 - a - 2
+    if num_spaces >= 0:
+        tmp += ' ' * num_spaces + SIGN
+    print(tmp.center(a))
+    i += 1
