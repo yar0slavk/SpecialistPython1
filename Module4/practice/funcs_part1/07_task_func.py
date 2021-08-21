a6fe8957fa0a7788529ea9e7b72e31da79568053
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-21 15:48:13 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,15 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def get_time(seconds):
+    hh24 = seconds // 3600
+    mi = (seconds - hh24 * 3600) // 60
+    ss = seconds % 60
+    return f'{hh24:0>2}:{mi:0>2}:{ss:0>2}'
+
+
+print(get_time(29143))
+print(get_time(13))
+print(get_time(120))
+print(get_time(3660))
