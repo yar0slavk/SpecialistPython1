b696021826010f4e35d2ccefcc1387d9abbda583
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-21 14:53:39 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -3,7 +3,30 @@
 # Если по заданным точкам треугольник построить нельзя, выведите соответствующее сообщение.
 # Подсказка: для нахождения площади используйте Теорему Герона
 
-# TODO: your code here
+def can_triangle(p1, p2, p3):
+    return not (p1 == p2 or p2 == p3 or p1[0] == p2[0] == p3[0] or p1[1] == p2[1] == p3[1])
 
 
-# Не забудьте протестировать вашу функцию
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def get_triangle_info(a, b, c):
+    if can_triangle(a, b, c):
+        dist_ab = distance(a[0], a[1], b[0], b[1])
+        dist_bc = distance(b[0], b[1], c[0], c[1])
+        dist_ac = distance(a[0], a[1], c[0], c[1])
+        p = dist_ab + dist_bc + dist_ac
+        p2 = p / 2
+        s = (p2 * (p2 - dist_ab) * (p2 - dist_bc) * (p2 - dist_ac)) ** 0.5
+        result = f'Периметр = {p:2f}\nПлощадь = {s:.2f}'
+    else:
+        result = 'по заданным точкам треугольник построить нельзя'
+    print(result)
+
+
+get_triangle_info((10, 12), (14, 18), (12, 12))
+print()
+get_triangle_info((0, 0), (2, 0), (0, 2))
+print()
+get_triangle_info((0, 0), (2, 0), (0, 0))
