532066e408c35c3b1327a0dca6032a73ecd6b1a1
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-21 16:46:17 +0300

    Update 11_task_func.py

--- Module4/practice/funcs_part2/11_task_func.py
+++ Module4/practice/funcs_part2/11_task_func.py
@@ -1,8 +1,7 @@
 # Используя функцию average() из предыдущей задачи, найдите среднее арифметическое всех элементов списка и кортежа
 
 def average(*args):
-    # TODO: тело из предыдущей задачи
-    pass
+    return sum(args)/len(args)
 
 
 def gen_list(size, at=-10, to=10):
@@ -13,11 +12,13 @@ def gen_list(size, at=-10, to=10):
     :param to: максимально возможное значение элементов
     :return: списко из size произвольных элементов вдиапазоне at..to 
     """
-    result_list = []
-    for _ in range(size):
-        result_list.append(random.randint(at, to))
-    return result_list
+    return [random.randint(at, to) for _ in range(size)]
 
 
 my_list = gen_list(10)
 my_tuple = 5, 7, -4, 10, 8
+
+print(my_list)
+print(average(*my_list))
+print(my_tuple)
+print(average(*my_tuple))
