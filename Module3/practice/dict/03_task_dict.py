42ad686d2d59bf94d6d1619decc4161239259ad1
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-25 22:06:49 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -23,26 +23,41 @@ staff = [
     {
         'name': 'Василий',
         'surname': 'Павлов',
-        'salary': 47800
+        'salary': 34500
     },
 ]
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
-
-# TODO: your code here
+# только один сотрудник
+max_emp = max(staff, key=lambda emp: emp['salary'])
+print(max_emp['name'], max_emp['surname'])
+# все сотрудники
+max_sal = max([emp['salary'] for emp in staff])
+max_emp = [f"{emp['name']} {emp['surname']}" for emp in staff if emp['salary'] == max_sal]
+print(f"({max_sal}) {', '.join(max_emp)}")
+print()
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
-
-# TODO: your code here
+# только один сотрудник
+min_emp = min(staff, key=lambda emp: emp['salary'])
+print(min_emp['name'], min_emp['surname'])
+# все сотрудники
+min_sal = min([emp['salary'] for emp in staff])
+min_emp = [f"{emp['name']} {emp['surname']}" for emp in staff if emp['salary'] == min_sal]
+print(f"({min_sal}) {', '.join(min_emp)}")
+print()
 
 print("Среднеарифметическую зарплату всех сотрудников")
-
-# TODO: your code here
+salaries = [emp['salary'] for emp in staff]
+print(sum(salaries) / len(salaries))
+print()
 
 print("Количество однофамильцев в организации")
-
-# TODO: your code here
+surnames = [emp['surname'] for emp in staff]
+namesakes = [sn for sn in surnames if surnames.count(sn) > 1]
+print(len(namesakes))
+print()
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
-
-# TODO: your code here
+staff_sorted = sorted(staff, key=lambda emp: emp['salary'])
+print('\n'.join([f"{emp['name']} {emp['surname']} ({emp['salary']})" for emp in staff_sorted]))
