007fde0511233356e26eed89286366fd052dfdc0
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-08-25 22:08:08 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -4,7 +4,34 @@
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
 def palindrome(number):
-    pass
+    return str(number) == str(number)[::-1]
+
+
+def get_digit_count(number):
+    if number > 0:
+        number = 1 + get_digit_count(number // 10)
+    return number
+
+
+def palindrome(number):
+    flag = True
+    digit_count = get_digit_count(number)
+    for i in range(digit_count // 2):
+        last_digit = number // (10 ** i) % 10
+        first_digit = number // (10 ** (digit_count - i - 1)) % 10
+        if last_digit != first_digit:
+            flag = False
+            break
+    return flag
+
+
+def palindrome(number):
+    reverse_number = 0
+    digit_count = get_digit_count(number)
+    for i in range(digit_count):
+        digit = number // (10 ** i) % 10
+        reverse_number += digit * (10 ** (digit_count - i - 1))
+    return reverse_number == number
 
 
 # Тестируем функцию
