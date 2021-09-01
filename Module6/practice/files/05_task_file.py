df98bb0d2c3ec1e6bcc43534975291c98b3fff4d
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-09-01 23:50:43 +0300

    Update 05_task_file.py

--- Module6/practice/files/05_task_file.py
+++ Module6/practice/files/05_task_file.py
@@ -6,3 +6,30 @@
 # 2. Какова выручка магазина по каждому типу товаров
 # 3. Какой тип товара был продан на самую большую сумму
 # 4. Сколько различных типов товаров было продано за день
+
+import os
+
+path = os.path.join('data', 'items_sold.txt')
+with open(path, 'r', encoding='UTF-8') as f:
+    sales = {}
+    total_sum = 0
+    for line in f:
+        for sale in line.split():
+            good, price = sale.split(':')
+            prices = sales.setdefault(good, [])
+            prices.append(float(price))
+            total_sum += float(price)
+
+print('1. Какова общая выручка магазина?')
+print(f'   {total_sum:.2f}')
+
+print('2. Какова выручка магазина по каждому типу товаров?')
+for good, prices in sales.items():
+    print(f'   {good:11}: {sum(prices):.2f}')
+
+print('3. Какой тип товара был продан на самую большую сумму?')
+max_price_sale = max(sales.items(), key=lambda x: x[-1])
+print(f'   {max_price_sale[0]}')
+
+print('4. Сколько различных типов товаров было продано за день?')
+print(f'   {len(sales)}')
