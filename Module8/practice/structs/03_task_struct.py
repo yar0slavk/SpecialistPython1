5ba26f1e7da07ea9aafda4f2168a19946366e339
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-09-04 15:09:55 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,21 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+
+
+def create_list(size, of=0, to=100):
+    return [random.randint(of, to) for _ in range(size)]
+
+
+lst = create_list(8, of=-10, to=20)
+print(lst)
+
+cnt = len([el for el in lst if el <= 10])
+print(cnt)
+
+s = sum([el for el in lst if el > 0])
+print(s)
+
+even_lst = [el for el in lst if el % 2 == 0]
+print(sum(even_lst) / len(even_lst))
