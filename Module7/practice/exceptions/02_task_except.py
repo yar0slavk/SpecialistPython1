f1102667baa53d716373c2f59a5468557e177e8c
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-09-04 11:59:58 +0300

    Update 02_task_except.py

--- Module7/practice/exceptions/02_task_except.py
+++ Module7/practice/exceptions/02_task_except.py
@@ -4,3 +4,35 @@
 # Алгоритм проверки на високосный год оформите в виде отдельной функции.
 #
 # Входная строка содержит два целых числа – номер месяца (возможно, неправильный) и номер года.
+import sys
+
+
+def is_leap_year(num_year):
+    return num_year % 4 == 0 and num_year % 100 != 0 or num_year % 400 == 0
+
+
+while True:
+    try:
+        input_data = input('Номер месяца и номер года (через пробел): ')
+        month, year = input_data.split()
+        month = int(month)
+        year = int(year)
+        if not (1 <= month <= 12):
+            raise ValueError
+        break
+    except ValueError:
+        print("Введены некоррекные данные, попробуйте снова")
+    except Exception as e:
+        print("Зачем же так грубо? -)", e)
+        sys.exit()
+
+if month in (1, 3, 5, 7, 8, 10, 12):
+    days = 31
+elif month in (4, 6, 9, 11):
+    days = 30
+elif month == 2 and is_leap_year(year):
+    days = 29
+else:
+    days = 28
+
+print(f'В месяце №{month} {year} года содержится {days} дней')
