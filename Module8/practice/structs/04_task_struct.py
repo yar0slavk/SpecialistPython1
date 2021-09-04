f6c32ba00ad1b0a2ede2b2ae546895d10c480e31
yar0slavk <88583654+yar0slavk@users.noreply.github.com>
2021-09-04 15:39:42 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,11 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+dictionary = dict(tuple(dictionary_1.items()) + tuple(dictionary_2.items()))
+print(dictionary)
+
+dictionary = {**dictionary_1, **dictionary_2}
+print(dictionary)
+
+dictionary = dictionary_1 | dictionary_2
+print(dictionary)
